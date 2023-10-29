024d87d15507e0ecd4a4f65a47be08badc999738
Shanky1001 <sr1071995@gmail.com>
2023-10-29 23:11:59 +0530

    test with numbers

--- sum.test.js
+++ sum.test.js
@@ -1,7 +1,28 @@
 // import { sum } from "./sum";
-const {sum} = require("./sum")
+const { sum } = require("./sum")
 
-test('Check basic sum', () => { 
-    const res = sum(2,3);
-    expect(res).toBe(5)
- })
+describe('Testing Sum function with numbers', () => {
+    test('Check basic sum', () => {
+        const res = sum(2, 3);
+        expect(res).toBe(5)
+    })
+    //  To equal Matcher
+    test("object Assignment", () => {
+        const obj = {};
+        expect(obj).toEqual({})
+    })
+    // Truthy and falsy matcher
+    test("Truthy or falsy", () => {
+        // const val = undefined;
+        // expect(val).toBeFalsy()
+        const val = 45;
+        expect(val).toBeTruthy()
+    })
+
+    // Less than or greater than matcher
+    test("Truthy or falsy", () => {
+        const val = sum(47, 45);
+        expect(val).toBeGreaterThan(50)
+        expect(val).toBeLessThan(100)
+    })
+})
